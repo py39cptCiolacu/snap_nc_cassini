9a5804be72a6023119e17877bfc31158ece8fd72
DanielCosmin <dciolacu87@yahoo.com>
2024-09-15 00:03:28 +0300

    end point - get user info

--- nc_lab/pdf_generator.py
+++ nc_lab/pdf_generator.py
@@ -1,7 +1,13 @@
 import pdfkit
 from jinja2 import Template
+from datetime import datetime
+import random
+import os
+import string
 
-def html_to_pdf(html_content: str, output_pdf_path) -> None:
+from .nc_analyzer import calc_min_max_by_day
+
+def html_to_pdf(html_content: str, output_pdf_path) -> bool:
 
     options = {
         "page-size" : "A4",
@@ -13,10 +19,14 @@ def html_to_pdf(html_content: str, output_pdf_path) -> None:
                        output_pdf_path, 
                        options=options)
     
+    return True
+    
 
-def generate_html() -> str:
+def generate_html(min_val_table_data: list[float], max_val_table_data: list[float]) -> str:
 
-    with open("./templates/2m_temperature.html", 'r', encoding='utf-8') as file:
+    TEMPLATE_PATH = "./nc_lab/tempaltes/2m_temperature.html"
+
+    with open(TEMPLATE_PATH, 'r', encoding='utf-8') as file:
         html_content= file.read()
     
     template = Template(html_content)
@@ -26,7 +36,26 @@ def generate_html() -> str:
     )
 
 
-output_pdf_path = "output.pdf"
-html = generate_html() 
-print(type(html))
-html_to_pdf(html ,output_pdf_path)
+def generate_random_filename() -> str:
+    """
+    Generates a random file name with the following format:
+    YYYYMMDD_XXXX
+    """
+    current_date = datetime.now().strftime("%Y%m%d")
+    random_suffix = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+    filename = f"{current_date}_{random_suffix}"
+
+    return filename
+
+def generate_pdf(nc_file_name: str) -> str:
+
+    min_val_table_data, max_val_table_data = calc_min_max_by_day("t2m", nc_file_name)
+    print(min_val_table_data)
+    html = generate_html(min_val_table_data, max_val_table_data)
+    pdf_name = nc_file_name[:-3] + ".pdf"
+    pdf = html_to_pdf(html, pdf_name)
+
+    if pdf:
+        return pdf_name 
+
+    return "Unkown error"
--- website/views.py
+++ website/views.py
@@ -1,15 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, request
+
+from nc_lab.nc_getter import create_dict, request_nc
+from nc_lab.pdf_generator import generate_pdf, generate_random_filename
 
 views = Blueprint("views", __name__)
-
-@views.route("/test")
-def test():
-
-    return "<h1> Salut </h1>"
 
 
 @views.route("/api/get_user_interest", methods=["GET"])
 def get_user_interest():
+
+    data = request.json
+    date = data.get("date")
+    land = data.get("land")
+
+    user_dict = create_dict(date = date, land = land)
+    random_file_name = generate_random_filename()
+    nc_file = request_nc(initial_dict=user_dict, file_name=random_file_name)
+
+    pdf_file = generate_pdf(nc_file)
 
     return
 
